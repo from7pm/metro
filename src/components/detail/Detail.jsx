a9639ca1b1fd7fb013712377be2a5217dc65c322
yhee <from7pm@gmail.com>
2026-07-23 15:26:46 +0900

    Restore previous state

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -155,8 +155,16 @@ function Detail() {
     dispatch(convenienceInfoIndex());
   }, []);
 
-  const currentConvenienceInfo = Array.isArray(convenienceInfo) ? convenienceInfo.find(info =>
-    info.STATION_NAME === station && (matchesLineField(info.LINE) || matchesLineField(info.LINE?.toString?.()))) || convenienceInfo.find(info => info.STATION_NAME === station) : null;
+  // const currentConvenienceInfo = Array.isArray(convenienceInfo)
+  //   ? convenienceInfo.find(info =>
+  //     info.STATION_NAME === station && (matchesLineField(info.LINE) || matchesLineField(info.LINE?.toString?.())))
+  //     || convenienceInfo.find(info => info.STATION_NAME === station)
+  //   : null;
+  const currentConvenienceInfo = Array.isArray(convenienceInfo)
+    ? convenienceInfo.find(info =>
+      info.STATION_NAME.includes(station) && (matchesLineField(info.LINE) || matchesLineField(info.LINE?.toString?.())))
+      || convenienceInfo.find(info => info.STATION_NAME === station)
+    : null;
 
   const getConvenienceInfo = (key) => {
     const value = currentConvenienceInfo?.[key];
@@ -187,10 +195,7 @@ function Detail() {
 
   const currentStationInfo = Array.isArray(stationInfo)
     ? stationInfo.find(info =>
-        info.SBWY_STNS_NM.includes(station) && (
-          matchesLineField(info.SBWY_ROUT_LN) || matchesLineField(info.SBWY_ROUT_LN?.toString?.())
-        )
-      ) 
+      info.SBWY_STNS_NM.includes(station) && (matchesLineField(info.SBWY_ROUT_LN) || matchesLineField(info.SBWY_ROUT_LN?.toString?.())))
     : null;
 
 
@@ -268,7 +273,7 @@ function Detail() {
           <div className="arrival-title">
             <p>도착 정보</p>
             {/* <button type="button" className='refresh-btn' onClick={handleRefresh} >🔄</button> */}
-            <img className={`refresh-btn ${refreshing ? "rotating" : ""}`} onClick={handleRefresh} src={`/icons/refresh-icon-2.svg`} alt="새로고침" />
+            <img className={`refresh-btn ${refreshing ? "rotating" : ""}`} onClick={handleRefresh} src={`/btn/refresh-btn.svg`} alt="새로고침" />
           </div>
           <div className="arrival-container">
             {Object.keys(groupedByDirection).length > 0 ? (
